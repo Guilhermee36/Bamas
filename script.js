2dd12d7bcc54a44a2bef4366e981c03b6bb172eb
Guilherme <guilhermelucario36@gmail.com>
2025-03-30 05:57:43 -0300

    menu Hamburguer

--- script.js
+++ script.js
@@ -211,3 +211,15 @@ document.addEventListener('DOMContentLoaded', function() {
     setupGalleryPage();
     setupContactPage();
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.querySelector(".burger");
+    const navLinks = document.querySelector(".nav-links");
+
+    if (burger && navLinks) {
+        burger.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
+            burger.classList.toggle("toggle");
+        });
+    }
+});
